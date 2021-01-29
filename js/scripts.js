f28cfa24e8133a90f2081f78f4ac4e4ce1e1dcc1
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-29 13:23:11 -0800

    Dynamically create Topping list for user

--- js/scripts.js
+++ js/scripts.js
@@ -14,25 +14,30 @@ Pizza.prototype.finalPrice = function() {
   return this.price;
 }
 
-const choices = [1, 2, 3];
-choices.forEach(function(choice) {
-    $('.toppings').append(`<p>Your choice is ${choice}</p>`);
-  })
-  // let pizza = new Pizza('large', ['cheese', 'pepperoni']);
-  // let price = pizza.finalPrice();
-  // console.log(price);
+// let pizza = new Pizza('large', ['cheese', 'pepperoni']);
+// let price = pizza.finalPrice();
+// console.log(price);
 
-// $(document).ready(function() {
-//   $('form').submit(function(event1) {
-//     event1.preventDefault();
-//     const size = $('input:radio[name=size]:checked').val();
-//     let toppings = [];
-//     $('input:checkbox[name=choice].checked').each(function() {
-//       let topping = $(this).val();
-//       toppings.push(topping);
-//     });
-//     let pizza = new Pizza(size, toppings);
-//     const price = pizza.finalPrice();
-//     console.log(price);
-//   });
-// });
+$(document).ready(function() {
+  const choices = ['Pepperoni', 'Mushrooms', 'Onions', 'Sausage', 'Bacon', 'Extra Cheese', 'Olives', 'Pineapple'];
+  choices.forEach(function(choice) {
+    $('.toppings').append(`<div class="form-check">
+    <input class="form-check-input" type="checkbox" name="choice" value="${choice}">
+    <label class="form-check-label" for="${choice}">
+      ${choice}
+    </label>
+  </div>`);
+  })
+  $('form').submit(function(event1) {
+    event1.preventDefault();
+    const size = $('input:radio[name=size]:checked').val();
+    let toppings = [];
+    $('input:checkbox[name=choice].checked').each(function() {
+      let topping = $(this).val();
+      toppings.push(topping);
+    });
+    let pizza = new Pizza(size, toppings);
+    const price = pizza.finalPrice();
+    console.log(price);
+  });
+});
