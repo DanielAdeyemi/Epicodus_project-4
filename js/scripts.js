18e97e0a6a96b64cd41eb810e3cb58005436eb08
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-29 10:04:13 -0800

    create prototype method for calculating price

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.price = 7;
-}
+};
 
 let pizza = new Pizza('small', ['cheese', 'pepperoni']);
-console.log(pizza);
+
+Pizza.prototype.finalPrice = function(size, toppings) {
+  this.price += (size + toppings);
+  return this.price;
+}
+
+let price = pizza.finalPrice(1, 1);
+console.log(price);
