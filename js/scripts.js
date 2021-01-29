15003ee50a04c11a8dc5a64d179760ec287e0add
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-29 10:41:25 -0800

    pass test 3: adjudst price with 1 topping

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,19 @@ function Pizza(size, toppings) {
 let pizza = new Pizza('small', ['cheese', 'pepperoni']);
 
 Pizza.prototype.finalPrice = function(size, toppings) {
-  // if (size === 'medium') {
-  //   size = 2;
-  // } else if (size === 'large') {
-  //   size = 4;
-  // } else size = 0;
-
-  // this.price += (size + toppings);
-  // return this.price;
-  size = size;
-  toppings = toppings;
-  return `This is a ${size} pizza with ${toppings} for $${this.price}.`
+  if (size === 'medium') {
+    size = 2;
+  } else if (size === 'large') {
+    size = 4;
+  } else size = 0; // {} were omiited intentionally - we can do it if statement has only 1 line of code
+  if (toppings.length > 0) topping = 1;
+  // topping = 0;
+  // toppings.forEach(function() {
+  //   topping += 1;
+  // })
+  this.price += (size + topping);
+  return this.price;
 }
 
-let price = pizza.finalPrice('small', ['cheese', 'pepperoni']);
+let price = pizza.finalPrice('medium', ['cheese']);
 console.log(price);
