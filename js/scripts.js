d3b4c7466347a1b49000e25c5fc849dad7f0567a
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-29 10:17:24 -0800

    pass test 1: display selection of size and toppings + current price

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,18 @@ function Pizza(size, toppings) {
 let pizza = new Pizza('small', ['cheese', 'pepperoni']);
 
 Pizza.prototype.finalPrice = function(size, toppings) {
-  if (size === 'medium') {
-    size = 2;
-  } else if (size === 'large') {
-    size = 4;
-  } else size = 0;
+  // if (size === 'medium') {
+  //   size = 2;
+  // } else if (size === 'large') {
+  //   size = 4;
+  // } else size = 0;
 
-  this.price += (size + toppings);
-  return this.price;
+  // this.price += (size + toppings);
+  // return this.price;
+  size = size;
+  toppings = toppings;
+  return `This is a ${size} pizza with ${toppings} for $${this.price}.`
 }
 
-let price = pizza.finalPrice('large', 0);
+let price = pizza.finalPrice('small', ['cheese', 'pepperoni']);
 console.log(price);
