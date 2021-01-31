9a98a0b112db6a70d03282aea6bf9a5069d78224
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-31 12:11:35 -0800

    replace if-else statement in UIL with ternary operators

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
   choices.forEach(function(choice) {
     $('.toppings .card-text').append(
       `<div class="form-check">
-      <input class="form-check-input" type="checkbox" name="choice" value="${choice}">
-      <label class="form-check-label" for="${choice}">${choice}</label>
+        <input class="form-check-input" type="checkbox" name="choice" value="${choice}">
+        <label class="form-check-label" for="${choice}">${choice}</label>
       </div>`);
     $('#next').click(function(event1) {
       event1.preventDefault();
@@ -38,9 +38,9 @@ $(document).ready(function() {
 
     let pizza = new Pizza(size, toppings);
     const price = pizza.finalPrice();
-    if (pizza.toppings.length !== 0)
-      $('#price').html(`Your choice is <span class="output">${size} size</span> pizza with <span class="output">${toppings}</span> as additional ingridient(s). <p id="finalCoast">Cost: $${price}.</p>`);
-    else $('#price').text(`Your choice is ${size} size pizza with no additional ingridients. Cost: $${price}.`);
+    pizza.toppings.length !== 0 ?
+      $('#price').html(`Your choice is <span class="output">${size} size</span> pizza with <span class="output">${toppings}</span> as additional ingridient(s). <p id="finalCoast">Cost: $${price}.</p>`) :
+      $('#price').text(`Your choice is ${size} size pizza with no additional ingridients. Cost: $${price}.`);
     $('#tops, .btn-primary').hide();
     $('.alert').show();
     $('.btn-light').click(function(event3) {
