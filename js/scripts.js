36fdd0bf1cd4cccee39dd958a26946736b61ea6e
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-31 12:07:58 -0800

    replace if-else statement in prototype method with ternary operators

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ function Pizza(size, toppings) {
 };
 
 Pizza.prototype.finalPrice = function() {
-  if (this.size === 'Medium') size = 2; // {} were omitted intentionally - we can do it if {if-else} condition has only 1 line of code
-  else if (this.size === 'Large') size = 4;
-  else size = 0;
+  size = (this.size === 'Medium') ? 2 :
+    (this.size === 'Large') ? 4 :
+    0;
   const topping = this.toppings.length;
   this.price += (size + topping);
   return this.price;
@@ -39,7 +39,7 @@ $(document).ready(function() {
     let pizza = new Pizza(size, toppings);
     const price = pizza.finalPrice();
     if (pizza.toppings.length !== 0)
-      $('#price').html(`Your choice is <span class="output">${size} size</span> pizza with <span class="output">${toppings}</span> as additional ingridient(s). Cost: $${price}.`);
+      $('#price').html(`Your choice is <span class="output">${size} size</span> pizza with <span class="output">${toppings}</span> as additional ingridient(s). <p id="finalCoast">Cost: $${price}.</p>`);
     else $('#price').text(`Your choice is ${size} size pizza with no additional ingridients. Cost: $${price}.`);
     $('#tops, .btn-primary').hide();
     $('.alert').show();
